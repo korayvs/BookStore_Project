4bb0ce1e49e00069935db126e2c274b91eb52128
Koray Vatansever <koray-vatansever@hotmail.com>
2025-02-17 18:13:30 +0300

    Add project files.

---
+++ BookStore.DataAccessLayer/Context/BookStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccessLayer.Context
+{
+    public class BookStoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-F1A12T8\\KORAY;initial catalog=ApiBookDb;integrated security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
+//trustservercertificate=true;
+//Plurualize
